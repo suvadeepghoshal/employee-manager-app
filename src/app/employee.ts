b4bc63ad5daf6664d127e1b0ba77cde87aa65b24
suvadeepghoshal <ghoshalsuvadeep594@gmail.com>
2021-08-13 09:16:00 +0530

    added employee model

---
+++ src/app/employee.ts
@@ -0,0 +1,10 @@
+// Employee model from the back end
+export interface Employee {
+    employeeId: number;
+    employeeName: string;
+    employeeEmail: string;
+    employeeJobTitle: string;
+    employeePhoneNumber: string;
+    employeeProfilePictureUrl: string;
+    employeeRandomCode: string;
+}
