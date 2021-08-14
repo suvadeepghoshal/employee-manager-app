7591fa02a6116815f4781f8333eca37a85fecadb
suvadeepghoshal <ghoshalsuvadeep594@gmail.com>
2021-08-14 15:29:58 +0530

    added a grid system for seeing employees

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCommonModule } from '@angular/material/core';
 import { MatCardModule } from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,7 +18,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    MatCommonModule, MatCardModule
+    MatCommonModule, MatCardModule, MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
